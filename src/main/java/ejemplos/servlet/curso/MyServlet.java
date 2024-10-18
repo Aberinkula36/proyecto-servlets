d56f40516dcc26965b980b0a4683c9dd4b393b02
Aberinkula36 <jmsaezteruel@hotmail.com>
2024-10-18 21:55:14 +0200

    desarrollo servlets: se ha modificado el proyecto para que al introducir un nombre en la web, se muestre en la siguiente ventana al clicar en "submit"

--- src/main/java/ejemplos/servlet/curso/MyServlet.java
+++ src/main/java/ejemplos/servlet/curso/MyServlet.java
@@ -19,7 +19,7 @@ public class MyServlet extends HttpServlet {
 
 		// send HTML page to client
 		out.println("<html>");
-		out.println("<head><title>Ejemplo HTML desde Servlet</title></head>");
+		out.println("<head><title>Ejemplo HTML desde Servlet </title></head>");
 		out.println("<body>");
 		out.println("<h1>Ejemplo Servlet</h1>");
 		out.println("<p>Este es un ejemplo en el curso de Java para generar HTML desde un Servlet.</p>");
